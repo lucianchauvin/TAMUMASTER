94c6098ca19484e0fe7820645844e28efe790815
Lucian Chauvin <lucianchauvin@gmail.com>
2022-09-19 15:00:02 -0500

    poggers:

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ zone = pytz.timezone('US/Central')
 f = open('bigData.json', 'r')
 data = json.load(f)
 f.close()
+data = [y for sub in [data.get(x) for x in data] for y in sub]
 
 
-def sortData(subject=None, courseNum=None, courseTitle=None, building=None, room=None, current=False, today=False, beginTime=None, endTime=None, days=None, seats=None, teacher=None, teacherEmail=None):
+def sortData(data, subject=None, courseNum=None, courseTitle=None, building=None, room=None, current=False, today=False, beginTime=None, endTime=None, days=None, seats=None, teacher=None, teacherEmail=None):
+    sorted = data
     if (beginTime != None and endTime != None) or today or current or days != None:
-        sorted = []
-        timeBypass = 0
+        sortedTemp = []
 
         dayDict = {'Sunday': 0, 'Monday': 1, 'Tuesday': 2,
                    'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6}
@@ -35,18 +36,36 @@ def sortData(subject=None, courseNum=None, courseTitle=None, building=None, room
                 dayClas = days
             else:
                 dayClas = [1,1,1,1,1,1,1]
-        if beginTime == None and endTime == None:
-            timeBypass = 1
         
-        for i in data:
-            for j in data.get(i):
-                if j['beginTime'] != None and j['endTime'] != None and beginTime != None and endTime != None:
-                    if int(j['beginTime']) < endTime and int(j['endTime']) > beginTime and list(j['days']) == dayClas and not timeBypass:
-                        sorted.append(j)
-                elif timeBypass and list(j['days']) == dayClas:
-                    sorted.append(j)
-    
+        for j in sorted:
+            if j['beginTime'] != None and j['endTime'] != None and current:
+                if int(j['beginTime']) < endTime and int(j['endTime']) > beginTime and list(j['days'])[dayDict[day]]:
+                    sortedTemp.append(j)
+            elif today:
+                if list(j['days'])[dayDict[day]]:
+                    sortedTemp.append(j)
+            else:
+                if list(j['days']) == dayClas:
+                    sortedTemp.append(j)
+
+        sorted = sortedTemp
+    if subject != None:
+        sortedTemp = [x for x in sorted if x['subject'] == subject]
+        sorted = sortedTemp
+    if courseNum != None:
+        sortedTemp = [x for x in sorted if x['courseNum'] == courseNum]
+        sorted = sortedTemp
+    if building != None:
+        sortedTemp = [x for x in sorted if x['building'] == building]
+        sorted = sortedTemp
+    if room != None:
+        sortedTemp = [x for x in sorted if x['room'] == room]
+        sorted = sortedTemp
+    if teacher != None:
+        sortedTemp = [x for x in sorted if teacher in x['teacher']]
+        sorted = sortedTemp
+
     return sorted
 
-print(sortData(today=True))
-        
+print(sortData(data, teacher="Haydon"))
+
